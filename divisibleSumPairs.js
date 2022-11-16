107db787c546861b690e3d9acbd8f999452f1e4b
MACBOOK <macbook@Vic.local>
2022-11-16 23:47:21 +0100

    FF

--- divisibleSumPairs.js
+++ divisibleSumPairs.js
@@ -4,6 +4,7 @@
 
 function divisibleSumPairs(n, k, ar) {
     // Write your code here
+    // Write your code here
     let counter = 0
     for (let i = 0; i < n; i++) {
         for (let j = i + 1; j < n; j++) {
