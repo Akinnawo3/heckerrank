3edaf45d8f5770a3b47eaddb2e806a2e82c7ffae
Akinnawo3 <akinnawovictor@gmail.com>
2022-11-21 12:55:58 +0100

    FF

--- catsAndMouse.js
+++ catsAndMouse.js
@@ -1,1 +1,8 @@
-// https://www.hackerrank.com/challenges/cats-and-a-mouse/problem?isFullScreen=true&fbclid=IwAR0wNk5KI2_Yi33vdFzJpuJLJ-KBb7GOj-hG_nXDWTgOXYhK6K0JISoElvY
+// https://www.hackerrank.com/challenges/cats-and-a-mouse/problem?isFullScreen=true&fbclid=IwAR0wNk5KI2_Yi33vdFzJpuJLJ-KBb7GOj-
+
+function catAndMouse(x, y, z) {
+  const distanceCatA = Math.abs(z - x);
+  const distanceCatB = Math.abs(z - y);
+
+  return distanceCatA < distanceCatB ? "Cat A" : distanceCatB < distanceCatA ? "Cat B" : "Mouse C";
+}
