13a7399eff7d36147606e73675c83a8596224ae8
Akinnawo3 <akinnawovictor@gmail.com>
2022-11-21 12:56:29 +0100

    cats and a mouse

--- catsAndMouse.js
+++ catsAndMouse.js
@@ -1,5 +1,6 @@
 // https://www.hackerrank.com/challenges/cats-and-a-mouse/problem?isFullScreen=true&fbclid=IwAR0wNk5KI2_Yi33vdFzJpuJLJ-KBb7GOj-
 
+
 function catAndMouse(x, y, z) {
   const distanceCatA = Math.abs(z - x);
   const distanceCatB = Math.abs(z - y);
